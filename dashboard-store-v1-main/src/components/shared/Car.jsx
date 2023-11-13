f83777df63e57253a2b09e42600bc45d95829d8c
Heriberto-ing <142918003+Heriberto-ing@users.noreply.github.com>
2023-11-13 18:42:08 -0500

    Update Car.jsx

--- dashboard-store-v1-main/src/components/shared/Car.jsx
+++ dashboard-store-v1-main/src/components/shared/Car.jsx
@@ -23,8 +23,60 @@ const Card = (props) => {
             <h5>Cant.</h5>
             <h5>Price</h5>
           </div>
-         
         </div>
+
+        <div className="h-[400px] md:h-[700px] lg:h-[540px] overflow-scroll">
+          <div className="count-products"></div>
+            <div className="container-cart-products-hidden-cart">
+              <div className="row-product">
+                <div className="cart-product">
+                  <div className="info-cart-product">
+                    <span className="cantidad-producto-carrito">1</span>
+                    <p className="titulo-producto-carrito">Martillos de ebanista</p>
+                    <span className="precio-producto-carrito">$8.000</span>
+                  </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon-close">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+               </div>
+               <div className="cart-product">
+                  <div className="info-cart-product">
+                    <span className="cantidad-producto-carrito">2</span>
+                    <p className="titulo-producto-carrito">STANLEY No.2 6 PULG</p>
+                    <span className="precio-producto-carrito">$4.500</span>
+                  </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon-close">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+               </div>
+               <div className="cart-product">
+                  <div className="info-cart-product">
+                    <span className="cantidad-producto-carrito">1</span>
+                    <p className="titulo-producto-carrito">Martillo para carpintería</p>
+                    <span className="precio-producto-carrito">$12.000</span>
+                  </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon-close">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+               </div>
+               <div className="cart-product">
+                  <div className="info-cart-product">
+                    <span className="cantidad-producto-carrito">1</span>
+                    <p className="titulo-producto-carrito">TALADRO DE ROTACION</p>
+                    <span className="precio-producto-carrito">$25.000</span>
+                  </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="icon-close">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+               </div>
+             </div>
+            
+              <div className="cart-total">
+                <h3>Total:</h3>
+                <span className="total-pagar">$54.000</span>
+              </div>
+           </div>
+          </div>
         {/* Submit payment */}
         <div className="bg-[#262837] absolute w-full bottom-0 left-0 p-4">
           <div className="flex items-center justify-between mb-4">
@@ -33,7 +85,7 @@ const Card = (props) => {
           </div>
           <div className="flex items-center justify-between mb-6">
             <span className="text-gray-400">Subtotal</span>
-            <span>$</span>
+            <span>$54.000</span>
           </div>
           <div>
             <button className="bg-[#ec7c6a] w-full py-2 px-4 rounded-lg">
