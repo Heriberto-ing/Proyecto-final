3b7ffa7d01960dc52846d78e70bbaba5836c9802
Heriberto-ing <142918003+Heriberto-ing@users.noreply.github.com>
2023-11-13 17:00:45 -0500

    Update App.jsx

--- dashboard-store-v1-main/src/App.jsx
+++ dashboard-store-v1-main/src/App.jsx
@@ -31,27 +31,11 @@ function App() {
     <div className="bg-[#262837] w-full min-h-screen">
       <Sidebar showMenu={showMenu} />
       <Car showOrder={showOrder} setShowOrder={setShowOrder} />
-      {/* Menu movil */}
-      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
-        <button className="p-2">
-          <RiUser3Line />
-        </button>
-        <button className="p-2">
-          <RiAddLine />
-        </button>
-        <button onClick={toggleOrders} className="p-2">
-          <RiPieChartLine />
-        </button>
-        <button onClick={toggleMenu} className="text-white p-2">
-          {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
-        </button>
-      </nav>
+     
       <main className="lg:pl-32 lg:pr-96 pb-20">
         <div className="md:p-8 p-4">
           {/* Header */}
           <Header/>
-          {/* Title content */}
-          
           {/* Content */}
           <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
             {/* Card */}
